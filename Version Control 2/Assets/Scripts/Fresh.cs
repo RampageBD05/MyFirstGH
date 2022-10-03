5706f26b67e6dcc2af6ca92d970e4e90c7aa8306
RampageBD05 <113905358+RampageBD05@users.noreply.github.com>
2022-10-03 14:25:48 +0200

    Created a script

---
+++ Version Control 2/Assets/Scripts/Fresh.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fresh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Freshly made
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
